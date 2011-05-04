c64f1307f54f21cc374a0b9cc6434b29f08768c6
Gavin Hurlbut <ghurlbut@mythtv.org>
2011-05-03 18:02:11 -0700

    Script to find non-existant or zero-byte recordings
    This will be run from a mythsystem hook of REC_STARTED.  If the file doesn't
    exist in 30s, it belches.  If the file isn't > 0 bytes within 10s of existance,
    it belches.
    
    To be added: add a "delete recording and allow re-record" message, and email
    me for notification.  Later on, one the hardware's ready, also power cycle
    the HD-PVR if it's the failing source.

---
+++ scripts/find-zero-byte.pl
@@ -0,0 +1,91 @@
+#! /usr/bin/perl
+
+use warnings;
+use strict;
+
+use lib '/opt/mythtv/new-logging/share/perl/5.10.1';
+
+use MythTV;
+
+my @hdpvrs = ( 32 );
+
+my $Myth = new MythTV();
+
+# $0 cardid chanid starttime
+my ($cardid, $chanid, $starttime) = @ARGV;
+
+die "You must provide cardid, chanid, starttime!\n"
+   if (!$cardid || !$chanid || !$starttime);
+
+my $recording = undef;
+my $delay = 30;
+
+while ((!$recording || !(exists $recording->{'local_path'}) || 
+        !(-f $recording->{'local_path'})) && $delay )
+{
+    $recording = $Myth->new_recording($chanid, myth_to_unix_time($starttime));
+
+    if (!$recording || !(exists $recording->{'local_path'}) || 
+        !(-f $recording->{'local_path'}) )
+    {
+        sleep 5;
+        $delay -= 5;
+        if (!$delay) {
+            no_recording($cardid, $chanid, $starttime);
+            exit(1);
+        }
+    }
+}
+
+# If we get here, we have a recording.
+if (-f $recording->{'local_path'})
+{
+    # And it's local.  Wait 10s if 0 bytes, then check again for 0 byte size.
+    my $delay = 1;
+    my $filesize = 0;
+
+    while( !$filesize && $delay >= 0 )
+    {
+        my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+            $atime,$mtime,$ctime,$blksize,$blocks) = 
+                stat($recording->{'local_path'});
+        $filesize = $size;
+
+        if ($size != 0)
+        {
+            exit(0);
+        }
+
+        sleep 10;
+        $delay--;
+    }
+
+    # #@$$@%ing zero byte file!
+    zero_byte($cardid, $chanid, $starttime);
+    exit(1);
+}
+
+exit 0;
+
+
+sub no_recording
+{
+    my ($cardid, $chanid, $starttime) = @_;
+
+    my $re = qr{^$cardid$};
+    my $ishdpvr = grep $re, @hdpvrs;
+    print "No recording for channel $chanid at $starttime\n";
+    print "Recording " . ($ishdpvr ? "" : "not ") . "from HD-PVR.\n";
+}
+
+sub zero_byte
+{
+    my ($cardid, $chanid, $starttime) = @_;
+
+    my $re = qr{^$cardid$};
+    my $ishdpvr = grep $re, @hdpvrs;
+    print "Zero byte recording for channel $chanid at $starttime\n";
+    print "Recording " . ($ishdpvr ? "" : "not ") . "from HD-PVR.\n";
+}
+
+# vim:ts=4:sw=4:ai:et:si:sts=4
